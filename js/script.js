4e140f04c8c64939895427383c0ff220039d41e2
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-11-27 16:44:28 +0100

    done

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ document.addEventListener('scroll',function(){
     let scrollPos=window.pageYOffset;
     if(scrollPos>100){header.style.backgroundColor="white";
     button.style.backgroundColor="#1a8917";
-}
+header.style.transition="background-color 1s ease;"
+button.style.transition="background-color 1s ease;"}
 else{header.style.backgroundColor=" #ffc017";
-button.style.backgroundColor="#191919";}});
+button.style.backgroundColor="#191919";
+header.style.transition="background-color 1s ease-out "
+button.style.transition="background-color 1s ease-out "}});
 
 
 console.log(window)
