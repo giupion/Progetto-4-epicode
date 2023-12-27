9bf7c3e79b7197f68cc012224eb1c4a047020645
Giuseppe Sansone <giuseppesansone144@gmail.com>
2023-12-27 18:13:10 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let header=document.querySelector("nav")
-let button=document.querySelector(" nav div>button")
+let button=document.querySelector(" nav ul li>button")
 document.addEventListener('scroll',function(){
 
     let scrollPos=window.pageYOffset;
